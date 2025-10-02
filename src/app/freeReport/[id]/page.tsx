a95ba71a96013a445ca9b18c41faf0024c4993a3
Astik <astemir.tlustangelov@gmail.com>
2025-10-02 15:14:50 +0300

    build errors|fonts

--- src/app/freeReport/[id]/page.tsx
+++ src/app/freeReport/[id]/page.tsx
@@ -266,7 +266,7 @@ const FreeReportPage: FC = () => {
                 <div className="w-8 h-8 border-2 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
               </div>
             ) : imageUrl ? (
-              <img
+              <Image
                 src={imageUrl}
                 alt="report"
                 width={900}
--- src/app/standartReport/[id]/page.tsx
+++ src/app/standartReport/[id]/page.tsx
@@ -420,7 +420,7 @@ const StandartReportPage = () => {
                 <div className="w-8 h-8 border-2 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
               </div>
             ) : imageUrl ? (
-              <img
+              <Image
                 src={imageUrl}
                 alt="report"
                 width={900}
